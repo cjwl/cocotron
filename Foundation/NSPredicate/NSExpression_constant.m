812b0eb2ce6892cf28c5d275499eafdfc6136638
Christopher Lloyd <cjwl@objc.net>
2008-05-15 14:48:01 +0000

    - NSExpression_constant retains constant value instead of copy per correct behavior

--- Foundation/NSPredicate/NSExpression_constant.m
+++ Foundation/NSPredicate/NSExpression_constant.m
@@ -11,7 +11,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 -initWithValue:value {
    [super initWithExpressionType:NSConstantValueExpressionType];
-   _value=[value copy];
+   _value=[value retain]; // not copy despite the object being "constant"
    return self;
 }
 
