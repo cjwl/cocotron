3d8541ca5bb7d35f5663b71f240ad967c494dc46
Johannes Fortmann <johannes.fortmann@googlemail.com>
2008-08-05 16:31:26 +0000

    now why did I call that thing KeyedUnarchiver? also licence corrections

--- testing/UnitTests/PropertyListUnarchiving.h
+++ testing/UnitTests/PropertyListUnarchiving.h
@@ -9,15 +9,7 @@
 #import <SenTestingKit/SenTestingKit.h>
 
 
-@interface KVO : SenTestCase {
-	NSString *someKey;
-	NSMutableDictionary *dict;
-	NSString *lastObserved;
-	BOOL observerCalled;
+@interface PropertyListUnarchiving : SenTestCase {
 }
-@property (copy) NSString *someKey;
-@property (copy) NSString *lastObserved;
-@property (retain) NSMutableDictionary *dict;
-@property (readonly) NSString *derived;
-@property (readonly) NSString *newStyleDerived;
+
 @end
