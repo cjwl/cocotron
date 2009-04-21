65adac241851ef756f81f000ef973cbae034e8ab
Christopher Lloyd <cjwl@objc.net>
2009-04-21 18:48:23 +0000

    added objc_property.m

---
+++ Foundation/objc/objc_property.m
@@ -0,0 +1,11 @@
+#import <objc/runtime.h>
+
+const char *property_getAttributes(objc_property_t property){
+   // UNIMPLEMENTED
+   return NULL;
+}
+
+const char *property_getName(objc_property_t property) {
+   // UNIMPLEMENTED
+   return NULL;
+}
