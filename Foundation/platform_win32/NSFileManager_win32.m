1d07580b1c964a0779d7f2e605fff47595af6b06
Christopher Lloyd <cjwl@objc.net>
2009-05-22 15:55:49 +0000

    Issue #290 patch by Markus Hitter, implementation of -[NSFileManager changeCurrentDirectoryPath:] for windows

--- Foundation/platform_win32/NSFileManager_win32.m
+++ Foundation/platform_win32/NSFileManager_win32.m
@@ -1,4 +1,5 @@
 /* Copyright (c) 2006-2007 Christopher J. W. Lloyd
+                 2009 Markus Hitter <mah@jump-ing.de>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -259,6 +260,15 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
    Win32Assert("GetCurrentDirectory");
 
    return [NSString stringWithCharacters:path length:length];
+}
+
+-(BOOL)changeCurrentDirectoryPath:(NSString *)path {
+
+   if (SetCurrentDirectoryW([self fileSystemRepresentationWithPathW:path]))
+    return YES;
+   Win32Assert("SetCurrentDirectory");
+
+   return NO;
 }
 
 
