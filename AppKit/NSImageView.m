4fc62a2e498648506d996d576ab2b22657ebf63b
Christopher Lloyd <cjwl@objc.net>
2009-06-17 15:31:23 +0000

    Issue #306 patch by Markus Hitter, NSImageView methods (modified)

--- AppKit/NSImageView.m
+++ AppKit/NSImageView.m
@@ -1,4 +1,5 @@
 /* Copyright (c) 2006-2007 Christopher J. W. Lloyd
+                 2009 Markus Hitter <mah@jump-ing.de>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -8,11 +9,72 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #import <AppKit/NSImageView.h>
 #import <AppKit/NSImageCell.h>
+#import <AppKit/NSRaise.h>
 
 @implementation NSImageView
 
 +(Class)cellClass {
    return [NSImageCell class];
+}
+
+-(BOOL)allowsCutCopyPaste {
+   // Because cut, copy, paste isn't implemented yet ...
+   return NO;
+}
+
+-(BOOL)animates {
+   // Because animation isn't implemented yet ...
+   return NO;
+}
+
+-(NSImage *)image {
+   return [_cell image];
+}
+
+-(NSImageAlignment)imageAlignment {
+   return [_cell imageAlignment];
+}
+
+-(NSImageFrameStyle)imageFrameStyle {
+   return [_cell imageFrameStyle];
+}
+
+-(NSImageScaling)imageScaling {
+   return [_cell imageScaling];
+}
+
+-(BOOL)isEditable {
+   return [_cell isEditable];
+}
+
+-(void)setAllowsCutCopyPaste:(BOOL)allow {
+   if(allow!=NO)
+    NSUnimplementedMethod();
+}
+
+-(void)setAnimates:(BOOL)flag {
+   if(flag!=NO)
+    NSUnimplementedMethod();
+}
+
+-(void)setEditable:(BOOL)flag {
+   [_cell setEditable:flag];
+}
+
+-(void)setImage:(NSImage *)image {
+   [_cell setImage:image];
+}
+
+-(void)setImageAlignment:(NSImageAlignment)alignment {
+   [_cell setImageAlignment:alignment];
+}
+
+-(void)setImageFrameStyle:(NSImageFrameStyle)frameStyle {
+   [_cell setImageFrameStyle:frameStyle];
+}
+
+-(void)setImageScaling:(NSImageScaling)scaling {
+   [_cell setImageScaling:scaling];
 }
 
 @end
