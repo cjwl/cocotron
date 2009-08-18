a8339f678d94459a854ad5d67b151f9250a92a50
Christopher Lloyd <cjwl@objc.net>
2009-08-18 17:13:51 +0000

    minor fixes for previous commits

--- AppKit/NSApplication.h
+++ AppKit/NSApplication.h
@@ -8,6 +8,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #import <AppKit/NSResponder.h>
 #import <AppKit/AppKitExport.h>
+#import <AppKit/NSGraphics.h>
 
 @class NSWindow,NSImage,NSMenu, NSPasteboard, NSDisplay;
 
