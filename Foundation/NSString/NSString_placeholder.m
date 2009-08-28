193e0ee639724320ef0ee92f8bababa1d74504c6
Dr. Rolf Jansen <rj@cyclaero.com>
2009-08-28 13:10:29 +0000

    -[NSString_placeholder initwithBytes:length:encoding] fixed crash calling with NSMacOSRomanStringEncoding

--- Foundation/NSString/NSString_placeholder.m
+++ Foundation/NSString/NSString_placeholder.m
@@ -15,6 +15,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #import <Foundation/NSString_nextstep.h>
 #import <Foundation/NSString_isoLatin1.h>
 #import <Foundation/NSString_win1252.h>
+#import <Foundation/NSString_macOSRoman.h>
 #import <Foundation/NSStringFormatter.h>
 #import <Foundation/NSStringFileIO.h>
 #import <Foundation/NSRaise.h>
@@ -76,10 +77,13 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
      characters=NSUTF8ToUnicode(bytes,length,&resultLength,NULL);
      return NSString_unicodePtrNewNoCopy(NULL,characters,resultLength);
 
-	case NSWindowsCP1252StringEncoding:
+	 case NSWindowsCP1252StringEncoding:
      return NSString_win1252NewWithBytes(NULL,bytes,length);
 
-    case NSUTF16BigEndianStringEncoding:
+    case NSMacOSRomanStringEncoding:
+      return NSString_macOSRomanNewWithBytes(NULL,bytes,length);
+         
+      case NSUTF16BigEndianStringEncoding:
      characters=NSUnicodeFromBytesUTF16BigEndian(bytes,length,&resultLength);
      return NSString_unicodePtrNewNoCopy(NULL,characters,resultLength);
 
