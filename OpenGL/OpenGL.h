31c3129d217d508fc9f1de374c06eb787ab8aeb9
Christopher Lloyd <cjwl@objc.net>
2010-01-09 21:55:16 +0000

    OpenGL.framework header fixes

--- OpenGL/OpenGL.h
+++ OpenGL/OpenGL.h
@@ -1,10 +1,14 @@
 #import <OpenGL/gl.h>
+#ifdef WINDOWS
 #import <OpenGL/glext.h>
+#endif
 
 #import <OpenGL/CGLTypes.h>
 #import <OpenGL/CGLCurrent.h>
 
-CGL_EXPORT CGLError CGLCreateContext(CGLPixelFormatObj pixelFormat,CGLContextObj other,CGLContextObj *result);
+// CGLCreateContext is currently platform specific
+// CGL_EXPORT CGLError CGLCreateContext(CGLPixelFormatObj pixelFormat,CGLContextObj other,CGLContextObj *result);
+
 CGL_EXPORT CGLError CGLDestroyContext(CGLContextObj context);
 
 CGL_EXPORT CGLError CGLLockContext(CGLContextObj context);
