0f6475d8899ec32ba48d639bffe5b2fa7523ea13
Christopher Lloyd <cjwl@objc.net>
2010-03-02 14:31:59 +0000

    (fixes issue #509) by Gary Palter

--- Foundation/NSNotificationCenter/NSObjectToObservers.m
+++ Foundation/NSNotificationCenter/NSObjectToObservers.m
@@ -70,7 +70,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
        [observers removeObjectAtIndex:count];
 
      if([observers count]==0)
-      removeKeys[removeCount++]=object;
+      removeKeys[removeCount++]=key;
     }
    }
 
