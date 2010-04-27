182ecc3f0d83b8425769d59de694eb80634dacbf
Glenn Ganz <glenn.ganz@uptime.ch>
2010-04-27 11:14:19 +0200

    fix Linux build

--- objc/objc_class.m
+++ objc/objc_class.m
@@ -5,6 +5,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+#import <Foundation/NSRaiseException.h>
 #import "objc_class.h"
 #import "objc_sel.h"
 #import "objc_protocol.h"
@@ -22,7 +23,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #ifdef SOLARIS
 #import <stdarg.h>
 #endif
-#import <Foundation/NSRaiseException.h>
 
 #import <pthread.h>
 
