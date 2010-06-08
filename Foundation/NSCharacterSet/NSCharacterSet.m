35785ce0deab6db090002bf75607dbae4def1c41
Christopher Lloyd <cjwl@objc.net>
2010-06-08 10:09:26 -0400

    (fixes issue #563)

--- Foundation/NSCharacterSet/NSCharacterSet.m
+++ Foundation/NSCharacterSet/NSCharacterSet.m
@@ -1,12 +1,10 @@
-/* Copyright (c) 2006-2007 Christopher J. W. Lloyd
+/* Copyright (c) 2006-2007 Christopher J. W. Lloyd <cjwl@objc.net>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
-
-// Original - Christopher Lloyd <cjwl@objc.net>
 #import <Foundation/NSCharacterSet.h>
 #import <Foundation/NSData.h>
 #import <Foundation/NSBundle.h>
@@ -152,8 +150,9 @@ static NSCharacterSet *sharedSetWithName(Class cls,NSString *name){
     static NSString *setName = @"whitespaceAndNewlineCharacterSet";
     id set;
     if ( !(set = NSMapGet(nameToSet,setName))) {
-        unichar chars[7] = { 0x20, 0x09,  0x0A, 0x0B, 0x0C, 0x0D,  0x85 };
-        set = [NSCharacterSet characterSetWithCharactersInString:[NSString stringWithCharacters:chars length:7]];
+    // Doc.s do not mention 0xA0 but it is implemented as a member
+        unichar chars[8] = { 0x20, 0x09,  0x0A, 0x0B, 0x0C, 0x0D,  0x85, 0xA0 };
+        set = [NSCharacterSet characterSetWithCharactersInString:[NSString stringWithCharacters:chars length:8]];
         NSMapInsert(nameToSet,setName,set);
     }
     return set;
@@ -163,8 +162,9 @@ static NSCharacterSet *sharedSetWithName(Class cls,NSString *name){
     static NSString *setName = @"whitespaceCharacterSet";
     id set;
     if ( !(set = NSMapGet(nameToSet,setName))) {
-        unichar chars[2] = { 0x20, 0x09 };
-        set = [NSCharacterSet characterSetWithCharactersInString:[NSString stringWithCharacters:chars length:2]];
+    // Doc.s do not mention 0xA0 but it is implemented as a member
+        unichar chars[3] = { 0x20, 0x09, 0xA0 };
+        set = [NSCharacterSet characterSetWithCharactersInString:[NSString stringWithCharacters:chars length:3]];
         NSMapInsert(nameToSet,setName,set);
     }
     return set;
