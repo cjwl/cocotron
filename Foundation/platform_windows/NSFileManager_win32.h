b9ea4cdca200f40c2c3a03db479070423a4b3056
Christopher Lloyd <cjwl@objc.net>
2010-10-22 15:59:24 -0400

    NSFileManager_win32 fixes

--- Foundation/platform_windows/NSFileManager_win32.h
+++ Foundation/platform_windows/NSFileManager_win32.h
@@ -8,6 +8,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #import <Foundation/NSFileManager.h>
 
-@interface NSFileManager_win32 : NSFileManager
+@interface NSFileManager_win32 : NSFileManager {
+   id _delegate;
+}
 
 @end
