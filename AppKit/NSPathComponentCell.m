b3a9c7bf4d073633c40d1fde80c102d3fbb149c1
Christopher Lloyd <cjwl@objc.net>
2010-11-03 22:37:40 -0400

    NSPathControl, NSPathCell, NSPathComponentCell fixes for layout compatibility

--- AppKit/NSPathComponentCell.m
+++ AppKit/NSPathComponentCell.m
@@ -6,32 +6,27 @@
 
 @implementation NSPathComponentCell
 
-- (NSURL *) URL;
-{
-	return [[_URL retain] autorelease];
+-(NSURL *)URL {
+   return _URL;
 }
 
-- (void) setURL: (NSURL *) newURL;
-{
-	if (_URL != newURL) {
-		[_URL release];
-		_URL = [newURL retain];
-	}
+-(void)setURL:(NSURL *)value {
+   value=[value retain];
+   [_URL release];
+   _URL=value;
 }
 
-- (NSString *) description;
-{
-	return [NSString stringWithFormat: @"<%@: %p %@ URL: %@, Image: %@)>", [self class], self, [self title], _URL, [self image]];
+-(NSString *)description {
+   return [NSString stringWithFormat: @"<%@: %p %@ URL: %@, Image: %@)>", [self class], self, [self title], _URL, [self image]];
 }
 
-- (NSDictionary *) textAttributes;
-{
-	NSFont *font = [self font];
-	return [NSDictionary dictionaryWithObjectsAndKeys: font, NSFontAttributeName, nil];
+-(NSDictionary *) textAttributes {
+   NSFont *font = [self font];
+
+   return [NSDictionary dictionaryWithObjectsAndKeys: font, NSFontAttributeName, nil];
 }
 
-- (void) drawInteriorWithFrame:(NSRect)frame inView:(NSView *)control
-{
+-(void)drawWithFrame:(NSRect)frame inView:(NSView *)view {
 	NSDictionary *attributes = [self textAttributes];
 	NSString *title = [self stringValue];
 	NSSize textSize = [title sizeWithAttributes: attributes];
@@ -53,11 +48,13 @@
 	[title drawAtPoint: frame.origin withAttributes: [self textAttributes]];
 }
 
-- (NSSize) cellSize;
-{
+-(NSSize) cellSize {
 	NSSize textSize = [[self stringValue] sizeWithAttributes: [self textAttributes]];
-	if ([self image]) textSize.width += textSize.height;
-	textSize.width += 5;
+    
+	if ([self image])
+     textSize.width += textSize.height;
+     
+	textSize.width += 14;
 	
 	return textSize;
 }
