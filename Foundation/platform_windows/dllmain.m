40c29db3cb802fcd0df34161ce144c339469c253
Glenn Ganz <glenn.ganz@uptime.ch>
2010-11-05 09:13:55 +0100

    fix in DllMain

--- Foundation/platform_windows/dllmain.m
+++ Foundation/platform_windows/dllmain.m
@@ -14,13 +14,12 @@ int OBJCRegisterDLL(HINSTANCE handle);
 
 int APIENTRY DllMain(HINSTANCE handle,DWORD reason,LPVOID _reserved) {
     
-    __NSInitializeProcess(__argc, (const char **)__argv);
-
-   if(reason==DLL_PROCESS_ATTACH)
-    return OBJCRegisterDLL(handle);
-
-   if(reason==DLL_THREAD_DETACH){
-     return TRUE;
-   }
-   return TRUE;
+    if(reason==DLL_PROCESS_ATTACH) {
+        __NSInitializeProcess(__argc, (const char **)__argv);
+        return OBJCRegisterDLL(handle);
+    }
+    if(reason==DLL_THREAD_DETACH){
+        return TRUE;
+    }
+    return TRUE;
 }
