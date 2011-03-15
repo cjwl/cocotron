be6ee9e672ddba36b1c0ae4598ac4b83223b3487
Glenn Ganz <glenn.ganz@uptime.ch>
2011-03-15 14:39:22 +0100

    FreeBSD initialization

--- Foundation/platform_bsd/libmain.m
+++ Foundation/platform_bsd/libmain.m
@@ -6,15 +6,58 @@
  
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
-#import "libmain.h"
-#import <Foundation/NSProcessInfo.h>
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#import <Foundation/Foundation.h>
 
 
-FOUNDATION_EXPORT void __attribute__ ((constructor)) libmain(void)
+
+extern void __attribute__ ((constructor)) libmain(void)
 {
-    fprintf(stderr, "Warning during Cocotron initialization: Not fully implemented on this platform", p);
+    fprintf(stderr, "Warning during Cocotron initialization: Not fully implemented on this platform");
     fflush(stderr);
     
-    //TODO call __NSInitializeProcess(..,..);
+    
+    char args[sysconf(_SC_ARG_MAX)];
+    int managementInfoBase[4];
+    size_t len;
+    int argc;
+        
+    managementInfoBase[0] = CTL_KERN;
+    managementInfoBase[1] = KERN_PROC;
+    managementInfoBase[2] = KERN_PROC_ARGS;
+    managementInfoBase[3] = getpid();
+    
+    len = sizeof(args);
+    if (sysctl(managementInfoBase, 4, args, &len, NULL, 0) == -1) {
+        fprintf(stderr, "Warning during Cocotron initialization: Unable to read argument list of the process");
+        abort();
+    }
+    
+    int laststart = 0;
+    for(int i;i < len;i++) {
+        if(args[i] == '\0') {
+            
+            char* arg = strdup(args + laststart );//malloc(i - laststart + 1);
+            
+            //strncpy(arg, args + laststart, i - laststart + 1);
+            laststart = i + 1;
+            argc++;
+            fprintf(stderr, "Argument : [%s], laststart [%d] [%c] [args + laststart +1]= %d \n",  arg, laststart, args[laststart], args + laststart +1);
+            fflush(stderr); 
+        }
+    }
+    
+    //int argc,const char *argv[]
+    
+    
+    //__NSInitializeProcess(args,len);
     OBJCInitializeProcess();
 }
