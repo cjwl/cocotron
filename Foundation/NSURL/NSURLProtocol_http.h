2bbc066a78020b87c60dc115ab733f4e6c5f194a
rcl <robert.lorentz@gmail.com>
2011-06-05 01:23:28 -0400

    Fixing issue with Duplicate Member _cachedResponse in NSURLProtocolHTTP.h

--- Foundation/NSURL/NSURLProtocol_http.h
+++ Foundation/NSURL/NSURLProtocol_http.h
@@ -17,8 +17,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
    NSOutputStream *_outputStream;
    NSTimer        *_timeout;
    
-   NSCachedURLResponse *_cachedResponse;
-
 // output state
    NSMutableArray *_outputQueue;
    NSInteger       _outputNextOffset;
