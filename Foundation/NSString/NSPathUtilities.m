17707fdbde2768fa9ceb3991aae7ba8af5e222e9
Christopher Lloyd <cjwl@objc.net>
2011-09-21 08:37:28 -0400

    change use of slash behavior

--- Foundation/NSString/NSPathUtilities.m
+++ Foundation/NSString/NSPathUtilities.m
@@ -16,20 +16,27 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #import <string.h>
 #import <stdio.h>
 
+
+/*  The rule of slash is to always use / for slash. All calls to platform API's must convert between 
+    the / and the platform slash. All public API, down to the Objective-C C string image name calls must use /.
+    
+    As of now, this is not true, but that is where it is going. The main reason being that URL's
+    expect all paths to use /, and there is a trend in Cocoa code to mix filesystem path strings
+    and NSURL strings. If we accept \ in file system path strings at the Cocoa layer, you end up
+    with broken URL's.
+    
+    We understand \ when present in a string to be a /, but we don't put them in strings.
+    
+ */
+ 
 #define ISSLASH(X)  ((X)=='/' || (X)=='\\')
 
-#ifdef WIN32
-#define SLASH '\\'
-#define SLASHSTRING @"\\"
-#else
 #define SLASH '/'
-#define SLASHSTRING @"/"
-#endif
 
 @implementation NSString(NSStringPathUtilities)
 
 +(NSString *)pathWithComponents:(NSArray *)components {
-   return [components componentsJoinedByString:SLASHSTRING];
+   return [components componentsJoinedByString:@"/"];
 }
 
 -(NSArray *)pathComponents {
@@ -142,7 +149,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
    for(i=length;--i>=0;)
     if(ISSLASH(buffer[i])){
      if(i==0)
-      return SLASHSTRING;
+      return @"/";
      else if(i+1<length)
       return [NSString stringWithCharacters:buffer length:i];
     }
@@ -246,7 +253,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     [standardPath getCharacters:buffer];
     
     for (i = 0; i < length; i++) {
-        cleanedBuffer[cleanedN++] = ISSLASH(buffer[i]) ? SLASH : buffer[i];  // convert all slashes to platform standard
+        cleanedBuffer[cleanedN++] = buffer[i];
         
         if (ISSLASH(buffer[i])) {
             while (i+1 < length && ISSLASH(buffer[i+1])) {
