8b95e82744c404dba5c673e8beec4917004b1778
Airy ANDRE <airy.andre@free.fr>
2011-11-06 14:54:57 +0100

    Fixed CGDataConsumer header

--- CoreGraphics/CGDataConsumer.h
+++ CoreGraphics/CGDataConsumer.h
@@ -1,13 +1,14 @@
 /* Copyright (c) 2009 Christopher J. W. Lloyd <cjwl@objc.net>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #import <CoreGraphics/CoreGraphicsExport.h>
 #import <CoreFoundation/CFData.h>
+#import <CoreFoundation/CFURL.h>
 
 typedef struct O2DataConsumer *CGDataConsumerRef;
 
@@ -15,7 +16,7 @@ typedef size_t (*CGDataConsumerPutBytesCallback) (void *info, const void *buffer
 
 typedef void (*CGDataConsumerReleaseInfoCallback) (void *info);
 
-typedef struct O2DataConsumerCallbacks {
+typedef struct CGDataConsumerCallbacks {
 	CGDataConsumerPutBytesCallback putBytes;
 	CGDataConsumerReleaseInfoCallback releaseConsumer;
 } CGDataConsumerCallbacks;
@@ -23,5 +24,5 @@ typedef struct O2DataConsumerCallbacks {
 COREGRAPHICS_EXPORT CGDataConsumerRef CGDataConsumerCreateWithCFData(CFMutableDataRef data);
 COREGRAPHICS_EXPORT CGDataConsumerRef CGDataConsumerCreate(void *info, const CGDataConsumerCallbacks *callbacks);
 COREGRAPHICS_EXPORT CGDataConsumerRef CGDataConsumerCreateWithURL(CFURLRef url);
-	
+
 COREGRAPHICS_EXPORT void CGDataConsumerRelease(CGDataConsumerRef self);
