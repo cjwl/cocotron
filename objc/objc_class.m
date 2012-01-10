23725fedf5611efd8c384f080c0591b4ef6aea40
Pauli Ojala <pauli@lacquer.fi>
2012-01-10 15:16:33 +0200

    Added OBJCCacheMethodInClass fix from glennganz-cocotron1 branch, rev 737063fa995e

--- objc/objc_class.m
+++ objc/objc_class.m
@@ -276,6 +276,8 @@ size_t class_getInstanceSize(Class class) {
 
 Ivar class_getInstanceVariable(Class class,const char *variableName) {
    for(;;class=class->super_class){
+    if(class==Nil)
+           break;
     struct objc_ivar_list *ivarList=class->ivars;
     int i;
 
@@ -283,8 +285,6 @@ Ivar class_getInstanceVariable(Class class,const char *variableName) {
      if(strcmp(ivarList->ivar_list[i].ivar_name,variableName)==0)
       return &(ivarList->ivar_list[i]);
     }
-    if(class->isa->isa==class)
-     break;
    }
 
    return NULL;
@@ -315,17 +315,22 @@ static inline void OBJCCacheMethodInClass(Class class,struct objc_method *method
 
    if(check->method->method_name==NULL)
     check->method=method;
-   else {
-    OBJCMethodCacheEntry *entry=allocateCacheEntry();
-    
-    entry->method=method;
-    
+   else {    
       BOOL success=NO;
       while(!success)
       {
          intptr_t offset=0;
-         while(offset=check->offsetToNextEntry, ((void *)check)+offset!=NULL)
-            check=((void *)check)+offset;
+          do {
+              check=((void *)check)+offset;
+              if (method == check->method) {
+                  // already cached
+                  return;
+              }
+          } while (offset=check->offsetToNextEntry, ((void *)check)+offset!=NULL);
+
+          OBJCMethodCacheEntry *entry=allocateCacheEntry();
+          
+          entry->method=method;
 
          success=__sync_bool_compare_and_swap(&check->offsetToNextEntry, offset, ((void *)entry)-((void *)check));
       }
@@ -400,17 +405,12 @@ static inline IMP OBJCLookupAndCacheUniqueIdInClass(Class class,SEL selector){
 
    if((method=class_getInstanceMethod(class,selector))!=NULL){
 
-   /**
-	* Method caching is broken - it seems the cache is always missed causing it to grow and
-	* grow making the caching method take longer and longer. Commenting this code
-	* prevents apps from slowing down over time.
-	*
-    * // When msg_tracing is on we don't cache the result so there is always a cache miss
-    * // and we always get the chance to log the msg
-	* if(!msg_tracing)
-	*	OBJCCacheMethodInClass(class,method);
-	*/
-	   
+    // When msg_tracing is on we don't cache the result so there is always a cache miss
+    // and we always get the chance to log the msg
+
+    if(!msg_tracing)
+     OBJCCacheMethodInClass(class,method);
+    
     return method->method_imp;
    }
 
@@ -441,8 +441,34 @@ Ivar *class_copyIvarList(Class cls,unsigned int *countp) {
 }
 
 Method *class_copyMethodList(Class cls,unsigned int *countp) {
-   // UNIMPLEMENTED
-   return NULL;
+    int methodCount = 0;
+    void *iterator = 0;
+    struct objc_method_list *list;
+    Method *result = NULL;
+    int j = 0;
+    
+    while((list = class_nextMethodList(cls, &iterator)) != NULL)
+    {
+        methodCount += list->method_count;
+    }
+    
+    if(countp != NULL) {
+        *countp = methodCount;
+    }
+    
+    result=malloc(sizeof(Method)*methodCount);
+    iterator = 0;
+    
+    while((list = class_nextMethodList(cls, &iterator)) != NULL)
+    {
+        int mCount = list->method_count;
+        for(int i = 0;i < mCount; i++) {
+            result[j] = &list->method_list[i];
+            j++;
+        }
+    }
+    
+    return result;
 }
 
 objc_property_t *class_copyPropertyList(Class cls,unsigned int *countp) {
@@ -514,7 +540,7 @@ BOOL class_addIvar(Class cls,const char *name,size_t size,uint8_t alignment,cons
    if(objc_lookUpClass(cls->name) != Nil) {
      return NO;
    }
-   for(class=cls;(class->isa->isa==class);class=class->super_class){
+   for(class=cls;(class!=Nil);class=class->super_class){
      ivars=class->ivars;
      if(ivars){
        for(i=0;i<ivars->ivar_count;i++){
@@ -522,9 +548,6 @@ BOOL class_addIvar(Class cls,const char *name,size_t size,uint8_t alignment,cons
            return NO;           // name exists
          }
        }
-     }
-     if(class->isa->isa==class){
-       break;
      }
    }
     
@@ -618,13 +641,30 @@ BOOL class_addMethod(Class cls, SEL name, IMP imp, const char *types) {
 }
 
 BOOL class_addProtocol(Class cls,Protocol *protocol) {
-   // UNIMPLEMENTED
-   return NO;
+	//TODO: check if protocol already exists
+	
+	struct objc_protocol_list *protocolList = malloc(sizeof(struct objc_protocol_list));
+	protocolList->next = 0;
+	protocolList->list[0] = protocol;
+    protocolList->count = 1;
+	struct objc_protocol_list *protoList = cls->protocols;
+	struct objc_protocol_list *lastList = protoList;
+	while((protoList=protoList->next) != NULL) {
+		lastList = protoList;
+	}
+	lastList->next = protocolList;
+	return YES;
 }
 
 BOOL class_conformsToProtocol(Class class,Protocol *protocol) {
 
+   if (class == Nil) {
+        return NO;
+   }
    for(;;class=class->super_class){
+       if(class==Nil)
+           break;
+
     struct objc_protocol_list *protoList=class->protocols;
 
     for(;protoList!=NULL;protoList=protoList->next){
@@ -635,9 +675,6 @@ BOOL class_conformsToProtocol(Class class,Protocol *protocol) {
        return YES;
      }
     }
-
-    if(class->isa->isa==class)
-     break;
    }
 
    return NO;
@@ -746,7 +783,7 @@ void OBJCRegisterClass(Class class) {
 
    if(class->super_class==NULL){
      // Root class
-    class->isa->isa=class;
+    class->isa->isa=class->isa;
     class->isa->super_class=class;
     class->info|=CLASS_INFO_LINKED;
    }
@@ -767,21 +804,26 @@ void OBJCRegisterCategoryInClass(Category category,Class class) {
 
    for(protos=category->protocols;protos!=NULL;protos=protos->next){
     unsigned i;
-
-    for (i=0;i<protos->count;i++)
-     OBJCRegisterProtocol((OBJCProtocolTemplate *)protos->list[i]);
+	   
+	   for (i=0;i<protos->count;i++) {
+		   OBJCRegisterProtocol((OBJCProtocolTemplate *)protos->list[i]);
+		   //add protocols from category to class
+		   class_addProtocol(class, protos->list[i]);
+	   }
    }
 }
 
 static void OBJCLinkClass(Class class) {
    if(!(class->info&CLASS_INFO_LINKED)){
     Class superClass=objc_lookUpClass((const char *)class->super_class);
+    Class metaRoot=objc_lookUpClass((const char *)class->isa->isa);
 	
-    if(superClass!=NULL){
+    if(superClass!=NULL && metaRoot!= NULL){
      class->super_class=superClass;
      class->info|=CLASS_INFO_LINKED;
      class->isa->super_class=class->super_class->isa;
      class->isa->info|=CLASS_INFO_LINKED;
+     class->isa->isa=metaRoot->isa;
 	}
    }
 }
