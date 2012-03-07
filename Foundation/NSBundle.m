8be254a6b5e93f4c082083f10ec35be4abedc8d7
Tilo Prütz <tilo@pruetz.net>
2012-03-07 08:59:37 +0100

    Reformatted code in preparation of code change.

--- Foundation/NSBundle.m
+++ Foundation/NSBundle.m
@@ -215,30 +215,30 @@ NSModuleHandle NSLoadModule(const char *path) {
 }
 #else
 
-NSModuleHandle NSLoadModule(const char *path) {
-   NSModuleHandle handle;
+NSModuleHandle NSLoadModule(const char *path)
+{
+    NSModuleHandle handle;
 
-   // dlopen doesn't accept partial paths.
-   if (path[0] != '/' && path[0] != '.') {
-      char buf[MAXPATHLEN];
+    // dlopen doesn't accept partial paths.
+    if (path[0] != '/' && path[0] != '.') {
+        char buf[MAXPATHLEN];
 
-      if (getcwd(buf, MAXPATHLEN) != NULL) {
-          strncat(buf, "/", MAXPATHLEN);
-          strncat(buf, path, MAXPATHLEN);
-          path = buf;
-      }
-      else {
-          NSCLog("NSLoadModule: cannot find cwd and relative path specified");
-          return NULL;
-      }
-   }
+        if (getcwd(buf, MAXPATHLEN) != NULL) {
+            strncat(buf, "/", MAXPATHLEN);
+            strncat(buf, path, MAXPATHLEN);
+            path = buf;
+        } else {
+            NSCLog("NSLoadModule: cannot find cwd and relative path specified");
+            return NULL;
+        }
+    }
 
-   handle = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
-   if (handle == NULL){
-       NSCLog(NSLastModuleError());
-   }
+    handle = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
+    if (handle == NULL) {
+        NSCLog(NSLastModuleError());
+    }
 
-   return handle;
+    return handle;
 }
 #endif
 
@@ -743,7 +743,7 @@ static NSMapTable *pathToObject=NULL;
 	if ([type length] && [[name pathExtension] isEqualToString:type]) {
 		// Kill the type form the extension part if it's already there
 		name = [name stringByDeletingPathExtension];
-	}	
+	}
 
 	if(type && [type length]!=0)
     file=[[name stringByAppendingFormat:@"-%@",NSPlatformResourceNameSuffix] stringByAppendingPathExtension:type];
