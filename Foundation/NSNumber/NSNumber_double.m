289d5db414db591829804f0c18a59a630776a927
Christopher Lloyd <cjwl@objc.net>
2013-05-21 10:46:37 -0400

    fix

--- Foundation/NSNumber/NSNumber_double.m
+++ Foundation/NSNumber/NSNumber_double.m
@@ -1,6 +1,5 @@
 /* Copyright (c) 2006-2007 Christopher J. W. Lloyd
-   Copyright (c) 2009 Jens Ayton
- 
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -9,7 +8,13 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #import <Foundation/NSNumber_double.h>
 #import <Foundation/NSStringFormatter.h>
+
+#if __APPLE__
 #import <Foundation/NSNumber_double_const.h>
+#else
+#import <Foundation/NSNumber_double_const_impl.h>
+#endif
+
 
 NSNumber *NSNumber_doubleSpecial(double value)
 {
@@ -154,6 +159,9 @@ NSNumber *NSNumber_doubleNew(NSZone *zone,double value) {
 }
 
 @end
+
+// This code needs to be here after the @implementation. If it's not clang is generating the NSNumber_double_const
+// class reference with a "97" after it. Unknown reasons.
 
 NS_DECLARE_CLASS_SYMBOL(NSNumber_double_const);
 
