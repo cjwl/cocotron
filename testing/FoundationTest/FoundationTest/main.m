7f2fd404ac34459d0f434363ebda9691bbc15d1c
Christopher Lloyd <cjwl@objc.net>
2013-05-23 23:12:27 -0400

    simple Foundation test for Darwin

---
+++ testing/FoundationTest/FoundationTest/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  FoundationTest
+//
+//  Created by Christopher Lloyd on 5/23/13.
+//  Copyright (c) 2013 cocotron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    [NSAutoreleasePool new];
+        
+        // insert code here...
+        NSLog(@"Hello, World! %p",argv);
+        
+    return 0;
+}
+
